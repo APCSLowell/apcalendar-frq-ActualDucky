118ceaf0847f4722cb50ea5a5803facb0945dd02
ActualDucky <119700539+ActualDucky@users.noreply.github.com>
2024-04-29 21:53:19 -0700

    APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -13,8 +13,10 @@ public class APCalendar
   public static int numberOfLeapYears(int year1, int year2)
   { 
     int count = 0;
-    for (int y = year1; y <= year2; y++) {
-      if (isLeapYear(y)) {
+    for (int y = year1; y <= year2; y++) 
+    {
+      if (isLeapYear(y)) 
+      {
         count++;
       }
       return count;
